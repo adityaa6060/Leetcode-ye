754363890df112aeeaabb53a86f8ef8a9418df1f
Aditya Tanwar <adityatanwar13827@gmail.com>
2026-09-06 00:10:41 +0530

    Time: 0 ms (100%), Space: 8.7 MB (33.24%) - LeetHub

---
+++ 2469-convert-the-temperature/2469-convert-the-temperature.c
@@ -0,0 +1,15 @@
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+double* convertTemperature(double celsius, int* returnSize) {
+    double*result=(double*)malloc(2*sizeof(double));
+
+    double kelvin=celsius + 273.15;
+    double fahrenheit=celsius*1.80 + 32.00;
+
+    result[0]= kelvin;
+    result[1]= fahrenheit;
+
+    *returnSize =2;
+    return result;
+}
